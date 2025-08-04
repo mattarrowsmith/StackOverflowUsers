fc813c7905c69cadd81172f7c849290e1c115298
Matthew Arrowsmith <matthew_arrowsmith@next.co.uk>
2025-08-04 20:26:39 +0100

    Create initial user DTO

---
+++ StackOverflowUsers/UserDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UserModelDTO.swift
+//  StackOverflowUsers
+//
+//  Created by Arrowsmith, Matthew on 04/08/2025.
+//
+
+struct UserDTO: Codable {
+    let accountId: String
+    let displayName: String
+    let profileImage: String
+}
