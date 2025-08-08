482f24003f8fc9c06d8be33f615730b865fbc433
Matthew Arrowsmith <matthew_arrowsmith@next.co.uk>
2025-08-08 09:10:48 +0100

    add follow repository mock

---
+++ StackOverflowUsersTests/Mocks/FollowRepositoryMock.swift
@@ -0,0 +1,33 @@
+//
+//  FollowRepositoryMock.swift
+//  StackOverflowUsers
+//
+//  Created by Arrowsmith, Matthew on 08/08/2025.
+//
+@testable import StackOverflowUsers
+
+struct FollowRepositoryMock: FollowRepositoryProtocol {
+    let followedUserIds: Set<Int>
+    let error: Error?
+
+    init(followedUserIds: Set<Int>, error: Error? = nil) {
+        self.followedUserIds = followedUserIds
+        self.error = error
+    }
+
+    func fetchFollowedUserIds() async throws -> Set<Int> {
+        return followedUserIds
+    }
+    
+    func followUser(withId id: Int) async throws {
+        if let error {
+            throw error
+        }
+    }
+    
+    func unfollowUser(withId id: Int) async throws {
+        if let error {
+            throw error
+        }
+    }
+}
